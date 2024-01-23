6a6235e27c70cceaea7ee6cbcb4290bc7d0c97c1
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2024-01-23 20:06:14 +0500

    first commit

---
+++ app/(components)/Nav.jsx
@@ -0,0 +1,23 @@
+import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
+const Nav = () => {
+
+  return (
+    <nav className="flex justify-between bg-nav p-4">
+      <div className="flex items-center justify-between w-full space-x-4">
+        <Link href="/">
+          <FontAwesomeIcon icon={faHome} className="icon" />
+        </Link>
+        <Link href="/TicketPage/new">
+          <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+      </div>
+      <div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
